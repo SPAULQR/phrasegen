dfb462952a8ab018cb5d56faa4de3d37305c1afa
Perevalov Pavel <perevalovpaul@gmail.com>
2020-03-25 21:53:53 +0300

    Update commit

--- main.php
+++ main.php
@@ -1,54 +1,42 @@
 <?php
 
-
 $link = mysqli_connect("localhost", "root", "", "phrasegen")
 or die ("Ошибка " . mysqli_connect_error());
 
 $string = '{Пожалуйста,|Просто|Если сможете,} сделайте так, чтобы это {удивительное|крутое|простое|важное|бесполезное}' .
     ' тестовое предложение {изменялось {быстро|мгновенно|оперативно|правильно} случайным образом|менялось каждый раз}.';
 
-function match($string)
+function decision($string)
 {
-    if (!preg_match_all('/{([^{}]+)}/', $string, $matches)) {
-        return $string;
-    }
-
     $arr = [];
-
-    foreach ($matches[1] as $key => $match) {
-        $searchTerm = $matches[0][$key];
-
-        if (!array_key_exists($key, $arr)) {
-            $arr[$key] = [];
+    foreach ($string as $item) {
+        preg_match('/{([^{}]+)}/u', $item, $matches);
+        $temp = explode('|', $matches[1]);
+        foreach ($temp as $value) {
+            $arr[] = str_replace($matches[0], $value, $item);
         }
-
-        $e = explode('|', $match);
-
-        foreach ($e as $v) {
-            $buffer = str_replace($searchTerm, $v, $string);
-
-            if (strpos($buffer, "{") !== false) {
-                $arr[$key][] = match($buffer);
-            } else {
-                $arr[$key][] = $buffer;
-            }
-        }
+    }
+    if (!(strpos($arr[count($arr) - 1], "{") === false)) {
+        $arr = decision($arr);
     }
     return $arr;
 }
 
-$match = match($string);
-$flatten = new RecursiveIteratorIterator(new RecursiveArrayIterator($match));
-$values = [];
-
-foreach ($flatten as $v) {
-      if (!in_array($v, $values)){
-           $values[] = $v;
+$answer = [];
+if (strpos($string, '{') === false) {
+    $answer = $string;
+} else {
+    $input[] = $string;
+    $tempArr = decision($input);
+    foreach ($tempArr as $value) {
+        if (!in_array($value, $answer)) {
+            $answer[] = $value;
+        }
     }
 }
 
-foreach ($values as $value) {
-    $insert = "INSERT INTO `phrases` (`phrase`) VALUES ('$value')";
+foreach ($answer as $v) {
+    $insert = "INSERT INTO `phrases` (`phrase`) VALUES ('$v')";
     $result = mysqli_query($link, $insert) or die("Ошибка " . mysqli_error($link));
-    echo $value."\n";
+    echo $v . "\n";
 }
